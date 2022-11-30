3a9479271011c37a270aaa9ae289d0607a414ce6
Martina928 <tinalai101@gmail.com>
2022-11-30 18:17:42 +0800

    init and render restaurant data

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
+// require restaurant data here
+const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
@@ -15,7 +17,7 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 // start and listen on the Express server
