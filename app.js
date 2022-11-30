d63a50dab51ce55fcdc7fab41e1d3d753be70e80
Martina928 <tinalai101@gmail.com>
2022-12-01 02:16:13 +0800

    add search keyword function

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => 
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  )
+  
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
