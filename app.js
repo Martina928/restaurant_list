3f56487fb2506ea78fe139c05a607e76b8320e1a
Martina928 <tinalai101@gmail.com>
2022-12-01 16:42:33 +0800

    add cannot-found page and optimize search function

--- app.js
+++ app.js
@@ -20,19 +20,22 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
+app.get('/restaurant/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
+  const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(restaurant => 
     restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
     restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   )
-  
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  if (!restaurants.length) {
+    res.render('cannot-found', { keyword: keyword })
+  } else {
+    res.render('index', { restaurants: restaurants, keyword: keyword })
+  }
 })
 
 // start and listen on the Express server
